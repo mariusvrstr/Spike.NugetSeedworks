5924748528886bf03a2d15ea9571c593ce81672b
Marius Vorster <mariusv@inoxico.com>
2022-02-13 08:23:28 +0200

    Added nuspec

---
+++ Spike.NugetSeedworks/MathWorker.cs
@@ -0,0 +1,24 @@
+﻿namespace Spike.NugetSeedworks;
+
+public static class MathWorker
+{
+    public static int Sum(this int original, int additionNumber)
+    {
+        return original + additionNumber;
+    }
+
+    public static int Substract(this int original, int subtractNumber)
+    {
+        return original - subtractNumber;
+    }
+
+    public static decimal Devide(this decimal original, int divideNumber)
+    {
+        return original / divideNumber;
+    }
+
+    public static decimal Multiply(this decimal original, int multiplicationNumber)
+    {
+        return original / multiplicationNumber;
+    }
+}
